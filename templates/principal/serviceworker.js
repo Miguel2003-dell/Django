064a03e5c4883f8d21775394400ce0086ae8e6c7
kennya2709 <kennyorona27@gmail.com>
2024-10-15 19:13:40 -0600

    v2

--- templates/principal/serviceworker.js
+++ templates/principal/serviceworker.js
@@ -1,7 +1,22 @@
 console.log("Hola Alumnos");
+const url=["manifest.json","/"];
 
 globalThis.addEventListener("install", event => {
      caches.open("pwa").then(cache => {
          cache.addAll(["manifest.json", "/static/1.jpg", "/static/2.jpg", "/static/3.jpg", "/static/4.jpg", "/static/5.jpg",]);
      })
 })
+globalThis.addEventListener("fetch", (event) => {
+    console.log(´HTTP: ${event.request.url}´);
+    event.respondWith(
+        caches.match(event.request)
+        .then (response){
+            if(response) {
+                return response
+        }else{
+            return fetch (event.request)
+        
+        }
+    }
+    );
+});
